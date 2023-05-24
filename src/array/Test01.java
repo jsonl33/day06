debc66eae6d464fd45433b8c8f32a971bc580030
testuser <testuser@test.com>
2023-05-24 16:15:58 +0900

    foreach

---
+++ src/array/Test01.java
@@ -0,0 +1,22 @@
+package array;
+
+public class Test01 {
+	public static void main(String[] args) {
+		String [] s = new String[3];
+		double[] d = new double[3];
+		System.out.println(s[0]);
+		System.out.println(d[0]);
+		System.out.println("===============================");
+		String[] s1 = {"안녕","그래","반갑다"};
+		double[] d1 = {1.1,2.2,3.3};
+		for(int i = 0;i<s1.length;i++) {
+			System.out.println(s1[i]);
+		}
+		for(String i:s1) {
+			System.out.println(i);
+		}
+		for(double i:d1) {
+			System.out.println(i);
+		}
+	}
+}
